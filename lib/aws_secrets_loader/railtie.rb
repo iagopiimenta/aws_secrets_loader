dea1fbf5b84cf3871ed1b2712138282cc335135a
Thiago Anunciação <thiago.anunciacao@me.com>
2023-04-10 17:53:16 -0300

    only make the load if the env's are setup

--- lib/aws_secrets_loader/railtie.rb
+++ lib/aws_secrets_loader/railtie.rb
@@ -3,7 +3,7 @@
 module AwsSecretsLoader
   class Railtie < Rails::Railtie
     config.before_configuration do
-      AwsSecretsLoader::AwsFetcher.load
+      AwsSecretsLoader::AwsFetcher.load if (ENV['AWS_SECRETS_LOADER'] || ENV['AWS_SECRET_NAME'])
     end
   end
 end
